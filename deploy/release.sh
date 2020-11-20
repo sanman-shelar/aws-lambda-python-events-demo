47f7c19b1cf4095a871d7fdfa491ef273fd96e8e
Sanman Shelar <39533009+sanman-shelar@users.noreply.github.com>
2020-11-20 17:10:35 +0530

    added action to configure aws creds

--- deploy/release.sh
+++ deploy/release.sh
@@ -1,19 +1,12 @@
-aws_access_key=$1
-aws_secret_key=$2
-aws_region=$3
-
-#Set env vars
-export AWS_ACCESS_KEY_ID=aws_access_key
-export AWS_SECRET_ACCESS_KEY=aws_secret_key
-export AWS_DEFAULT_REGION=aws_region
-
 # Install required dependencies
 npm install aws-cdk
 pip3 install setuptools
 pip3 install -r requirements.txt
 
+node_modules/aws-cdk/bin/cdk bootstrap
+
 #Synthesize an AWS CloudFormation template
 node_modules/aws-cdk/bin/cdk synth
 
 #Deploy
-node_modules/aws-cdk/bin/cdk deploy
+node_modules/aws-cdk/bin/cdk deploy  --require-approval never
