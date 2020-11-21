27c554e982a8210117663d27245a1ba61605a357
Sanman Shelar <39533009+sanman-shelar@users.noreply.github.com>
2020-11-21 16:25:39 +0530

    testing code changes impact for stack

--- lambda/event.py
+++ lambda/event.py
@@ -2,16 +2,17 @@ import os
 import boto3
 import json
 
+
 def handler(event, context):
 
-    print('## ENVIRONMENT VARIABLES')
+    print("## ENVIRONMENT VARIABLES")
     print(os.environ)
-    print('## EVENT')
+    print("## EVENT")
     print(json.dumps(event))
 
-    sns = boto3.client('sns')
-    sns.publish(TopicArn=os.environ['PERSON_SNS_TOPIC_ARN'], Message=event['body'])
+    sns = boto3.client("sns")
+    sns.publish(TopicArn=os.environ["PERSON_SNS_TOPIC_ARN"], Message=event["body"])
 
-    return {
-        "statusCode": 202
-    }
+    print("Event Published to SNS")
+
+    return {"statusCode": 202}
